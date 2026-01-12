dbfdf19c99716c54f8f8ff2d5b69f2aa70057e74
QasimBoubekeur <qasim.boubekeur@proton.me>
2026-01-12 11:22:21 +0100

    MainScene: Add SimulationManager

---
+++ Sources/SDCTUIO/Assets/Scripts/SimulationManager.cs
@@ -0,0 +1,37 @@
+using System;
+using One_Sgp4;
+using UnityEngine;
+using DigitalRuby.Earth;
+
+public class SimulationManager : MonoBehaviour
+{
+    public const float EARTH_RADIUS_KM = 6371.0f;
+
+    [SerializeField]
+    private EarthScript Earth;
+    [SerializeField]
+    public float SimulationSpeed;
+
+    public static SimulationManager Instance { get; private set; }
+
+    public EpochTime SimulationTime { get; private set; }
+    public float SimulationScaleFactor { get; private set; }
+
+    void Awake() 
+    {
+        Instance = this;
+    }
+
+    void Start()
+    {
+        this.SimulationTime = new EpochTime(DateTime.UtcNow);
+        this.SimulationScaleFactor = Earth.Radius / EARTH_RADIUS_KM;
+
+        DebrisManager.Instance.AddDebrisToSimulation(DebrisData.TestDebrisData());
+    }
+
+    void Update()
+    {
+        this.SimulationTime.addTick(Time.deltaTime * this.SimulationSpeed);
+    }
+}
