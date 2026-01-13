a5cdbcfbd8d38742c270c9ba0d01a35c954d9c64
QasimBoubekeur <qasim.boubekeur@proton.me>
2026-01-13 15:32:02 +0100

    Debris: refactor position calculation

--- Sources/SDCTUIO/Assets/Scripts/DebrisController.cs
+++ Sources/SDCTUIO/Assets/Scripts/DebrisController.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class DebrisController : MonoBehaviour
 {
-    private Tle Tle { get; set; }
+    public Tle Tle { get; set; }
     public DebrisData DebrisData { get; private set; }
     private bool IsInitialized = false;
 
@@ -32,19 +32,11 @@ public class DebrisController : MonoBehaviour
             return;
         }
 
-        Sgp4Data sgp4DebrisData = SatFunctions.getSatPositionAtTime(
+        transform.position = Utils.GetUnityPosition(
             this.Tle,
             SimulationManager.Instance.SimulationTime,
-            Sgp4.wgsConstant.WGS_84
+            SimulationManager.Instance.SimulationScaleFactor
         );
-
-        // ATTENTION: Unity utilise un systeme de coordonnees Y-up tandis que SGP4 utilise Z-up
-        Point3d realPositionKm = sgp4DebrisData.getPositionData();
-        transform.position = new Vector3(
-            (float)realPositionKm.x,
-            (float)realPositionKm.z,
-            (float)realPositionKm.y
-        ) * SimulationManager.Instance.SimulationScaleFactor;
     }
 
     private void OnDebrisTapped(object sender, System.EventArgs e)
--- Sources/SDCTUIO/Assets/Scripts/Shared/Utils.cs
+++ Sources/SDCTUIO/Assets/Scripts/Shared/Utils.cs
@@ -1,5 +1,25 @@
+using UnityEngine;
+using One_Sgp4;
+
 public static class Utils
 {
+    public static Vector3 GetUnityPosition(Tle tle, EpochTime time, float scaleFactor)
+    {
+        Sgp4Data sgp4DebrisData = SatFunctions.getSatPositionAtTime(
+            tle,
+            time,
+            Sgp4.wgsConstant.WGS_84
+        );
+
+        // ATTENTION: Unity utilise un systeme de coordonnees Y-up tandis que SGP4 utilise Z-up
+        Point3d realPositionKm = sgp4DebrisData.getPositionData();
+        return new Vector3(
+            (float)realPositionKm.x,
+            (float)realPositionKm.z,
+            (float)realPositionKm.y
+        ) * scaleFactor;
+    }
+
     public static string AddTleChecksum(this string str)
     {
         int sum = 0;
