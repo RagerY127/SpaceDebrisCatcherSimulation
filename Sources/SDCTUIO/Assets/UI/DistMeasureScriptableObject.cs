129ea542f07772bd88976ca1c81f80b697eda8c7
vitawrap <thevitawrap@gmail.com>
2026-01-20 15:17:12 +0100

    MainUI: Barre de mesure de distances
    Avec databinding, mais il faut encore faire le vrai calcul avec ce databinding

---
+++ Sources/SDCTUIO/Assets/UI/DistMeasureScriptableObject.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "DistMeasureDataSource", menuName = "Scriptable Objects/DistMeasureScriptableObject")]
+public class DistMeasureScriptableObject : ScriptableObject
+{
+    public float ratio = 0f;
+}
