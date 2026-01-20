929b0eca087cae757a04c9a80060147dab3fddef
Adrian Maraj <adrian.maraj@yahoo.de>
2026-01-20 15:32:50 +0100

    UI: speed changes now

--- Sources/SDCTUIO/Assets/Scripts/UIController/SimulationBarController.cs
+++ Sources/SDCTUIO/Assets/Scripts/UIController/SimulationBarController.cs
@@ -7,6 +7,11 @@ public class SimulationBarController : MonoBehaviour
     private Button _playPauseButton;
     private Button _speedDownButton;
     private Button _speedUpButton;
+    private Label _speedLabel;
+
+    private readonly float[] _speedSteps = { 1f, 10f, 100f, 500f, 1000f };
+    private int _currentSpeedIndex = 0;
+    
 
     void OnEnable()
     {
@@ -14,6 +19,7 @@ public class SimulationBarController : MonoBehaviour
         _playPauseButton = root.Q<Button>("start_stop");
         _speedDownButton = root.Q<Button>("reverse");
         _speedUpButton = root.Q<Button>("forward");
+        _speedLabel = root.Q<Label>("speed");
 
         if (_playPauseButton != null)
         {
@@ -31,6 +37,7 @@ public class SimulationBarController : MonoBehaviour
         {
             _speedUpButton.clicked += OnSpeedUp;
         }
+        ApplySpeed();
     }
 
     private void OnToggleSimulation()
@@ -54,11 +61,54 @@ public class SimulationBarController : MonoBehaviour
 
     private void OnSpeedDown()
     {
+        if (!SimulationManager.Instance.IsSimulationRunning) return;
+
+        if (_currentSpeedIndex > 0)
+        {
+            _currentSpeedIndex--;
+            ApplySpeed();
+        }
         print("Slow down simulation");
     }
 
     private void OnSpeedUp()
     {
+        if (!SimulationManager.Instance.IsSimulationRunning) return;
+
+        if (_currentSpeedIndex < _speedSteps.Length - 1)
+        {
+            _currentSpeedIndex++;
+            ApplySpeed();
+        }
         print("Speed up simulation");
+    }
+
+    private void ApplySpeed()
+    {
+        float newSpeed = _speedSteps[_currentSpeedIndex];
+         
+        
+        if (_speedLabel != null)
+        {
+            _speedLabel.text = _speedSteps[_currentSpeedIndex].ToString() + "x";
+        }
+
+        if (_currentSpeedIndex == _speedSteps.Length -1 )
+        {
+            _speedUpButton?.AddToClassList("button--disabled");
+            if (_speedUpButton != null) _speedUpButton.pickingMode = PickingMode.Ignore;
+        }
+        else if (_currentSpeedIndex == 0)
+        {
+            _speedDownButton?.AddToClassList("button--disabled");
+            if (_speedDownButton != null) _speedDownButton.pickingMode = PickingMode.Ignore;
+        }
+        else
+        {
+            _speedDownButton?.RemoveFromClassList("button--disabled");
+            if (_speedDownButton != null) _speedDownButton.pickingMode = PickingMode.Position;
+            _speedUpButton?.RemoveFromClassList("button--disabled");
+            if (_speedUpButton != null) _speedUpButton.pickingMode = PickingMode.Position;
+        }
     }
 }
