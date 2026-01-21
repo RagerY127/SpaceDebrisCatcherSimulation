b1e9371ff5c51ae0cdf371cd9e29e1321118d2ab
QasimBoubekeur <qasim.boubekeur@proton.me>
2026-01-21 12:19:25 +0100

    SimulationManager: refactor random debris creation

--- Sources/SDCTUIO/Assets/Scripts/SimulationManager.cs
+++ Sources/SDCTUIO/Assets/Scripts/SimulationManager.cs
@@ -29,17 +29,9 @@ public class SimulationManager : MonoBehaviour
         this.SimulationTime = new EpochTime(System.DateTime.UtcNow);
         this.SimulationScaleFactor = Earth.Radius / EARTH_RADIUS_KM;
 
-        DebrisData testDebris = DebrisData.TestDebrisData(45.0f, 45.0f);
-
-        DebrisManager.Instance.AddDebrisToSimulation(DebrisData.TestDebrisData());
-        DebrisManager.Instance.AddDebrisToSimulation(DebrisData.TestDebrisData(90.0f, 0.0f, DebrisShape.Cylinder));
-        DebrisManager.Instance.AddDebrisToSimulation(DebrisData.TestDebrisData(49.0f, 155.0f));
-        DebrisManager.Instance.AddDebrisToSimulation(testDebris);
-        DebrisManager.Instance.SelectDebris(testDebris.Id);
-
         for (int i = 0; i < 20; i++)
         {
-            DebrisManager.Instance.AddDebrisToSimulation(DebrisData.TestDebrisData(Random.Range(-90.0f, 90.0f), Random.Range(-90.0f, 90.0f)));
+            DebrisManager.Instance.AddDebrisToSimulation(DebrisData.RandomDebris());
         }
     }
 
