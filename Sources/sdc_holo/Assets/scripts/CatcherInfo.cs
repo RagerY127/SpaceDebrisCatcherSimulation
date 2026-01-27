48e1dfc99531a11afe0e951c7ad7b2a7e221acb1
Wire0n <pierre1239@hotmail.fr>
2026-01-27 12:00:09 +0100

    prefab catcher info

---
+++ Sources/sdc_holo/Assets/scripts/CatcherInfo.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class CatcherInfo : MonoBehaviour
+{
+    [Header("CatcherInfo fields")]
+    public GameObject targetNameField;
+    public GameObject speedField;
+    public GameObject targetDistanceField;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
