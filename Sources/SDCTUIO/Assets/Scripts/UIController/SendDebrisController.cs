ca87d7249fd921b76b449cc827821c4cbae33144
Wire0n <pierre1239@hotmail.fr>
2026-02-02 11:49:22 +0100

    clear des objets

--- Sources/SDCTUIO/Assets/Scripts/UIController/SendDebrisController.cs
+++ Sources/SDCTUIO/Assets/Scripts/UIController/SendDebrisController.cs
@@ -27,9 +27,9 @@ public class SendDebrisController : MonoBehaviour
         {
             Debug.Log("The button is clicked, demande the server...");
 
-            HololensMessage message=new HololensMessage(MessageType.CATCHER);
+            HololensMessage message=new HololensMessage(MessageType.DEBRIS);
 
-            /*
+            
             ObjectDataBundle debris1=new ObjectDataBundle();
             debris1.data.Add(new ObjectDataEntry(){key="name",value="debrisCube"});
             debris1.data.Add(new ObjectDataEntry(){key="revolutionPerDay",value="4.5"});
@@ -37,8 +37,8 @@ public class SendDebrisController : MonoBehaviour
             debris1.data.Add(new ObjectDataEntry(){key="position",value="27.6"});
 
             message.messageData.Add(new DataEntry(){key="debris1",value=debris1});
-            */
-
+            
+            /*
             ObjectDataBundle catcher=new ObjectDataBundle();
             catcher.data.Add(new ObjectDataEntry(){key="targetName",value="debris1"});
             catcher.data.Add(new ObjectDataEntry(){key="speed",value="1542.36"});
@@ -51,7 +51,7 @@ public class SendDebrisController : MonoBehaviour
             debris1.data.Add(new ObjectDataEntry(){key="revolutionPerDay",value="4.5"});
             debris1.data.Add(new ObjectDataEntry(){key="mass",value="85.9"});
             debris1.data.Add(new ObjectDataEntry(){key="position",value="27.6"});
-            message.messageData.Add(new DataEntry(){key="debris1",value=debris1});
+            message.messageData.Add(new DataEntry(){key="debris1",value=debris1});*/
             
             string json=JsonUtility.ToJson(message);
             BridgeServer.Instance.SendMessageToHoloLens(json);
--- Sources/sdc_holo/Assets/scripts/BridgeClient.cs
+++ Sources/sdc_holo/Assets/scripts/BridgeClient.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Concurrent;
+using System.Collections.Generic;
 using System.Net.Sockets;
 using System.Text;
 using System.Threading;
@@ -28,8 +29,10 @@ public class BridgeClient : MonoBehaviour
     [Header("Paramètres de génération")]
     public GameObject debrisPrefab;
     public GameObject catcherPrefab;
+    public GameObject debrisInfo;//prefab qui affiche les données du débris
     public GameObject catcherInfo;//prefab qui affiche les données du catcher
 
+    private List<GameObject> spawnedObjects = new List<GameObject>();
     // File thread-safe pour transférer les messages du thread réseau vers le thread principal
     private ConcurrentQueue<string> messageQueue = new ConcurrentQueue<string>();
 
@@ -135,6 +138,17 @@ public class BridgeClient : MonoBehaviour
         TryHandleJsonMessage(msg);
         
     }
+    // ===============================
+    // Nettoyage des objets spawnés 
+    // ===============================
+    void ClearSpawnedObjects()
+    {
+        foreach (var obj in spawnedObjects)
+        {
+            Destroy(obj);
+        }
+        spawnedObjects.Clear();
+    }
 
     // ===============================
     // Génération du cube 
@@ -146,15 +160,17 @@ public class BridgeClient : MonoBehaviour
             if (Camera.main != null)
             {
                 Transform camTransform = Camera.main.transform;
-                Instantiate(
+                var obj = Instantiate(
                     debrisPrefab,
                     camTransform.position + camTransform.forward * 1.5f,
                     Quaternion.identity
                 );
+                spawnedObjects.Add(obj);
             }
             else
             {
-                Instantiate(debrisPrefab, new Vector3(0, 0, 1f), Quaternion.identity);
+                var obj = Instantiate(debrisPrefab, new Vector3(0, 0, 1f), Quaternion.identity);
+                spawnedObjects.Add(obj);
             }
 
             Debug.Log("Cube apparu !");
@@ -174,15 +190,17 @@ public class BridgeClient : MonoBehaviour
             if (Camera.main != null)
             {
                 Transform camTransform = Camera.main.transform;
-                Instantiate(
+                var obj = Instantiate(
                     catcherPrefab,
                     camTransform.position + camTransform.forward * 1.5f,
                     Quaternion.identity
                 );
+                spawnedObjects.Add(obj);
             }
             else
             {
-                Instantiate(catcherPrefab, new Vector3(0, 0, 1f), Quaternion.identity);
+                var obj = Instantiate(catcherPrefab, new Vector3(0, 0, 1f), Quaternion.identity);
+                spawnedObjects.Add(obj);
             }
 
             Debug.Log("Catcher apparu !");
@@ -208,6 +226,7 @@ public class BridgeClient : MonoBehaviour
                     camTransform.position - camTransform.forward * 0.2f,
                     Quaternion.identity
                 );
+                spawnedObjects.Add(pane);
                 var script=pane.GetComponent<CatcherInfo>();
                 if(script!=null)
                 script.UpdateInfo(targetName, speed, targetDistance);
@@ -219,6 +238,7 @@ public class BridgeClient : MonoBehaviour
             else
             {
                 GameObject pane=Instantiate(catcherInfo, new Vector3(0, 0, 1f), Quaternion.identity);
+                spawnedObjects.Add(pane);
                 var script=pane.GetComponent<CatcherInfo>();
                 if(script!=null)
                 script.UpdateInfo(targetName, speed, targetDistance);
@@ -229,6 +249,52 @@ public class BridgeClient : MonoBehaviour
             }
 
             Debug.Log("Infos du catcher apparues !");
+        }
+        else
+        {
+            Debug.LogError("Prefab non assigné dans l'inspecteur");
+        }
+    }
+
+    // ===============================
+    // Faire Apparaitre les données du débris
+    // ===============================
+
+    void SpawnDebrisData(string debrisName, double revolution, double mass, double position)
+    {
+         if (debrisInfo != null)
+        {
+            if (Camera.main != null)
+            {
+                Transform camTransform = Camera.main.transform;
+                GameObject pane=Instantiate(
+                    debrisInfo,
+                    camTransform.position - camTransform.forward * 0.2f,
+                    Quaternion.identity
+                );
+                spawnedObjects.Add(pane);
+                var script=pane.GetComponent<DebrisInfo>();
+                if(script!=null)
+                script.UpdateInfo(debrisName, revolution, mass, position);
+                else
+                {
+                    Debug.LogError("DebrisInfo script not found on the prefab.");
+                }
+            }
+            else
+            {
+                GameObject pane=Instantiate(debrisInfo, new Vector3(0, 0, 1f), Quaternion.identity);
+                spawnedObjects.Add(pane);
+                var script=pane.GetComponent<DebrisInfo>();
+                if(script!=null)
+                script.UpdateInfo(debrisName, revolution, mass, position);
+                else
+                {
+                    Debug.LogError("DebrisInfo script not found on the prefab.");
+                }
+            }
+
+            Debug.Log("Infos du débris apparues !");
         }
         else
         {
@@ -248,7 +314,14 @@ public class BridgeClient : MonoBehaviour
             Debug.Log("Type du message JSON = " + message.type);
             if(message.type == MessageType.DEBRIS)
             {
+                ObjectDataBundle catcher=message.messageData[0].value;
+                string name=(catcher.GetValue("name")==null)?"Unknown":catcher.GetValue("name");
+                double revolution=double.Parse((catcher.GetValue("revolutionPerDay")==null)?"0":catcher.GetValue("revolutionPerDay"),CultureInfo.InvariantCulture);
+                double mass=double.Parse((catcher.GetValue("mass")==null)?"0":catcher.GetValue("mass"),CultureInfo.InvariantCulture);
+                double position=double.Parse((catcher.GetValue("position")==null)?"0":catcher.GetValue("position"),CultureInfo.InvariantCulture);
+                ClearSpawnedObjects();
                 SpawnCube();
+                SpawnDebrisData(name, revolution, mass, position);
             }
             else if(message.type == MessageType.CATCHER)
             {
@@ -257,6 +330,7 @@ public class BridgeClient : MonoBehaviour
                 double speed=double.Parse((catcher.GetValue("speed")==null)?"0":catcher.GetValue("speed"),CultureInfo.InvariantCulture);
                 double distance=double.Parse(catcher.GetValue("targetDistance")==null?"0":catcher.GetValue("targetDistance"),CultureInfo.InvariantCulture);
                 
+                ClearSpawnedObjects();
                 SpawnCatcher();
                 SpawnCube();
                 SpawnCatcherData(name, speed, distance);
