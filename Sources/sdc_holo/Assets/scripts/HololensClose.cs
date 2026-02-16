dad13251c3479b076a452594a03789a5b82a81b2
Wire0n <pierre1239@hotmail.fr>
2026-02-16 15:23:57 +0100

    added a close bouton to clean hololens view from objects

---
+++ Sources/sdc_holo/Assets/scripts/HololensClose.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class HololensClose : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //update button position and angle based on camera position and angle
+        if (Camera.main != null)
+        {
+            Transform camTransform = Camera.main.transform;
+            transform.position = camTransform.position + camTransform.forward * 0.6f + camTransform.up * 0.2f - camTransform.right * 0.5f;
+            transform.rotation = camTransform.rotation;
+        }
+    }
+    public void onClick()
+    {
+        ObjectManager.ClearSpawnedObjects();
+    }
+}
